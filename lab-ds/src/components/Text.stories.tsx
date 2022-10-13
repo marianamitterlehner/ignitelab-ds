636e75f2a031dd09d42888f8f2d166af01b75382
Mariana <mariana.m.miguel@gmail.com>
2022-10-12 21:09:29 -0300

    first step completed

---
+++ lab-ds/src/components/Text.stories.tsx
@@ -0,0 +1,50 @@
+
+import { Text, TextProps } from './Text';
+import { Meta, StoryObj} from '@storybook/react';
+
+export default {
+    title: 'Components/Text',
+    component: Text,
+    args: {
+        children: 'Loren ipsum'
+    },
+    argTypes: {
+        size: {
+            options: ['sm', 'md', 'lg'],
+            control: {
+                type:'inline-radio'
+            }
+        }
+    }
+} as Meta<TextProps>
+
+//variações
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size:'sm'
+    }
+}
+
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size:'lg'
+    }
+}
+
+export const CustomComponet: StoryObj<TextProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <p>testando</p>
+        )
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true
+            }
+        }
+    }
+}
